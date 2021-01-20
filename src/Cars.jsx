e3e046cca507648c8dac938e0c37ef2e062bef63
Rodolfo <rodolfojr94@gmail.com>
2021-01-20 20:24:38 -0300

    refatorado addicionando useState

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,34 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { carBlue, carRed, carYellow } from './images';
 import contextLeftCars from './contextLeftCar';
 import Car from './Car';
 
-export default class Cars extends Component {
-  constructor() {
-    super();
-    this.state={
-      cars: { red: false, blue: false, yellow: false },
-    }
-    this.switchCar = this.switchCar.bind(this);
+export default function Cars() {
+  const initialState = { red: false, blue: false, yellow: false };
+  const [cars, setCars] = useState(initialState);
+  function switchCar({ target: { id }}) {
+    setCars(Object.assign({}, cars, { [id]: !cars[id] } ))
   }
 
-  switchCar({ target: { id }}) {
-    this.setState(({ cars }) => {
-      return ({ cars: { [id]: !cars[id] }, })
-    })
-  }
-
-  render() {
-    const LeftCars = {
-      switchCar: this.switchCar,
-      cars: this.state.cars,
-    }
-    return (
-      <contextLeftCars.Provider value={ LeftCars } >
-        <Car alt="red car" img={ carRed } />
-        <Car alt="blue car" img={ carBlue } />
-        <Car alt="yellow car" img={ carYellow } />
-      </contextLeftCars.Provider>
-    );
-  }
+  return (
+    <contextLeftCars.Provider value={ { switchCar, cars } } >
+      <Car alt="red car" img={ carRed } />
+      <Car alt="blue car" img={ carBlue } />
+      <Car alt="yellow car" img={ carYellow } />
+    </contextLeftCars.Provider>
+  );
 }
